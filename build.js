9b8c6db0f9e9c0e30719f143b7bc90f05fb60eb7
Paul Panserrieu <paul.panserrieu@gmail.com>
2014-07-08 10:42:08 +0200

    1.1.4

--- build.js
+++ build.js
@@ -1,7 +1,7 @@
 var HTTP = require("q-io/http"),
     FS = require("q-io/fs");
 
-HTTP.read('https://raw.github.com/peutetre/zepto/1.1.3/zepto.js')
+HTTP.read('https://raw.githubusercontent.com/peutetre/zepto/1.1.4/zepto.js')
     .then(function (body) {
         var rslt = body.toString()
                     .replace(/var Zepto =/, 'var Zepto = module.exports =')
